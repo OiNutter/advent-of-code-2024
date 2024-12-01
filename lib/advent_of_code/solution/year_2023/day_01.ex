9684c87ade2662fbd319c31b86966bc76131596e
Will McKenzie <willmckenzie@oinutter.co.uk>
2024-12-01 09:39:18 +0000

    add 2023 placeholders and day 1 solution

---
+++ lib/advent_of_code/solution/year_2023/day_01.ex
@@ -0,0 +1,41 @@
+defmodule AdventOfCode.Solution.Year2023.Day01 do
+
+  def getValue(match) do
+    %{
+      "one" => 1,
+      "two" => 2,
+      "three" => 3,
+      "four" => 4,
+      "five" => 5,
+      "six" => 6,
+      "seven" => 7,
+      "eight" => 8,
+      "nine" => 9
+    }
+    |> Map.get(match, match)
+  end
+
+  def getValues(input, regex) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      digits = Regex.scan(regex, line, capture: :all)
+      |> Enum.map(fn m ->
+        Enum.at(m, 1)
+      end)
+
+      String.to_integer(~s(#{getValue(List.first(digits))}#{getValue(List.last((digits)))}))
+    end)
+    |> Enum.sum()
+
+
+  end
+
+  def part1(input) do
+    getValues(input, ~r/(\d{1})/)
+  end
+
+  def part2(input) do
+    getValues(input, ~r/(?=(one|two|three|four|five|six|seven|eight|nine|\d){1})/)
+  end
+end
