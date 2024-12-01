98461368a04f8b91b4e37682e6ac222837566699
Will McKenzie <willmckenzie@oinutter.co.uk>
2024-12-01 09:38:16 +0000

    specify local dir for inputs

--- config/config.exs
+++ config/config.exs
@@ -3,8 +3,9 @@ import Config
 config :elixir, :time_zone_database, Tz.TimeZoneDatabase
 
 config :advent_of_code, AdventOfCode.Input,
-  # allow_network?: true,
-  session_cookie: System.get_env("ADVENT_OF_CODE_SESSION_COOKIE")
+  #allow_network?: true,
+  session_cookie: System.get_env("ADVENT_OF_CODE_SESSION_COOKIE"),
+  cache_dir: "inputs"
 
 # If you don't like environment variables, put your cookie in
 # a `config/secrets.exs` file like this:
