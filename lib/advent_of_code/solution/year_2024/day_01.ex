ee60496b29b791341abe1aa0b0007756e4007223
Will McKenzie <willmckenzie@oinutter.co.uk>
2024-12-02 14:20:04 +0000

    optimise 2024 day 1 solution

--- lib/advent_of_code/solution/year_2024/day_01.ex
+++ lib/advent_of_code/solution/year_2024/day_01.ex
@@ -1,21 +1,18 @@
 defmodule AdventOfCode.Solution.Year2024.Day01 do
 
   def get_pairs(input) do
-    input
+    {left, right} = input
     |> String.split("\n", trim: true)
-    |> Enum.reduce(%{left: [], right: []}, fn line, acc ->
-      digits = line
-      |> String.split("   ")
-      |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&String.split/1)
+    |> Enum.map(&List.to_tuple/1)
+    |> Enum.map(fn {a, b} -> {String.to_integer(a), String.to_integer(b)} end)
+    |> Enum.unzip()
 
-      left = Map.get(acc, :left) ++ [Enum.at(digits, 0)] |> Enum.sort()
-      right = Map.get(acc, :right) ++ [Enum.at(digits, 1)] |> Enum.sort()
-      %{left: left, right: right}
-    end)
+    {Enum.sort(left), Enum.sort(right)}
   end
 
   def part1(input) do
-    %{left: left, right: right} = get_pairs(input)
+    {left, right} = get_pairs(input)
 
     left
     |> Enum.with_index()
@@ -27,7 +24,7 @@ defmodule AdventOfCode.Solution.Year2024.Day01 do
   end
 
   def part2(input) do
-    %{left: left, right: right} = get_pairs(input)
+    {left, right} = get_pairs(input)
 
     left
     |> Enum.map(fn d ->
