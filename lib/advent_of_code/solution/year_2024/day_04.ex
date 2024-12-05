7ec3d4be6ae2920f1bbe260ca6cae5bc164fee9b
Will McKenzie <willmckenzie@oinutter.co.uk>
2024-12-05 21:40:51 +0000

    add days 4 and 5 to 2024

--- lib/advent_of_code/solution/year_2024/day_04.ex
+++ lib/advent_of_code/solution/year_2024/day_04.ex
@@ -1,7 +1,84 @@
 defmodule AdventOfCode.Solution.Year2024.Day04 do
-  def part1(_input) do
+   def get_xmas_regex(dist) do
+    {_, regex} = Regex.compile("(?=(X.{#{dist}}M.{#{dist}}A.{#{dist}}S))")
+    regex
   end
 
-  def part2(_input) do
+  def count_forward(string, line_length) do
+    [
+      get_xmas_regex(0)
+      |> Regex.scan(string)
+      |> length(),
+      get_xmas_regex(line_length)
+      |> Regex.scan(string)
+      |> length(),
+      get_xmas_regex(line_length+1)
+      |> Regex.scan(string)
+      |> length(),
+      get_xmas_regex(line_length-1)
+      |> Regex.scan(string)
+      |> length()
+    ]
+    |> Enum.sum()
+  end
+
+  def part1(input) do
+    line_length = input
+      |> String.split("\n", trim: true)
+      |> List.first()
+      |> String.length()
+    parsed = input
+    |> String.replace("\n", ".")
+
+    reversed = parsed
+    |> String.split("", trim: true)
+    |> Enum.reverse()
+    |> Enum.join("")
+
+    count_forward(parsed, line_length) + count_forward(reversed, line_length)
+  end
+
+  def part2(input) do
+    grid = input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      String.split(line, "", trim: true)
+    end)
+
+    grid
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {line, y}, count ->
+      line
+      |> Enum.with_index()
+      |> Enum.reduce(count, fn {char, x}, count ->
+        if char === "M" || char === "S" do
+          bottom_right_search = if char === "M", do: "S", else: "M"
+          # check top right corner
+          if Enum.at(line, x+2) === "M" || Enum.at(line, x+2) === "S" do
+            bottom_left_search = if Enum.at(line, x+2) === "M", do: "S", else: "M"
+            # check bottom left corner
+            if Enum.at(Enum.at(grid, y+2, []), x) === bottom_left_search do
+              # check bottom right corner
+              if Enum.at(Enum.at(grid, y+2,[]), x+2) === bottom_right_search do
+                # check middle
+                if Enum.at(Enum.at(grid, y+1,[]), x+1) === "A" do
+                  count + 1
+                else
+                  count
+                end
+              else
+                count
+              end
+            else
+              count
+            end
+          else
+            count
+          end
+        else
+          count
+        end
+      end)
+    end)
   end
 end
--- lib/advent_of_code/solution/year_2024/day_05.ex
+++ lib/advent_of_code/solution/year_2024/day_05.ex
@@ -1,7 +1,82 @@
 defmodule AdventOfCode.Solution.Year2024.Day05 do
-  def part1(_input) do
+  def get_rules_map(rules) do
+    rules
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(%{}, fn line, rules ->
+      {x, y} =
+        String.split(line, "|", parts: 2, trim: true)
+        |> List.to_tuple()
+
+      Map.update(rules, y, [], & &1)
+      |> Map.update(y, [], &[x | &1])
+    end)
   end
 
-  def part2(_input) do
+  def get_middle(update) do
+    middle =
+      update
+      |> length()
+      |> div(2)
+
+    Enum.at(update, middle)
+  end
+
+  def get_sorted(pages, rules_map) do
+    pages
+    |> Enum.sort(fn a, b ->
+      rules = Map.get(rules_map, a, [])
+      if Enum.member?(rules, b), do: false, else: true
+    end)
+  end
+
+  def check_correct(line, rules_map, want_correct \\ true) do
+      pages =
+        line
+        |> String.split(",", trim: true)
+
+      sorted = get_sorted(pages, rules_map)
+
+      if pages == sorted , do: want_correct, else: !want_correct
+  end
+
+  def part1(input) do
+    [rules, updates] =
+      input
+      |> String.split("\n\n", trim: true, parts: 2)
+
+    rules_map = get_rules_map(rules)
+
+    updates
+    |> String.split("\n", trim: true)
+    |> Enum.filter(fn line -> check_correct(line, rules_map) end)
+    |> Enum.map(fn update ->
+      pages =
+        String.split(update, ",", trim: true)
+        |> Enum.map(&String.to_integer/1)
+
+      get_middle(pages)
+    end)
+    |> Enum.sum()
+  end
+
+  def part2(input) do
+    [rules, updates] =
+      input
+      |> String.split("\n\n", trim: true, parts: 2)
+
+    rules_map = get_rules_map(rules)
+
+    updates
+    |> String.split("\n", trim: true)
+    |> Enum.filter(fn line -> check_correct(line, rules_map, false) end)
+    |> Enum.map(fn update ->
+      pages =
+        String.split(update, ",", trim: true)
+
+      get_sorted(pages, rules_map)
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.map(&get_middle/1)
+    |> Enum.sum()
   end
 end
