a04c6acae276c8482e69786851f2ea222d52f5ae
Will McKenzie <willmckenzie@oinutter.co.uk>
2024-12-11 11:24:01 +0000

    add day 11 2024

--- lib/advent_of_code/solution/year_2024/day_11.ex
+++ lib/advent_of_code/solution/year_2024/day_11.ex
@@ -1,7 +1,47 @@
 defmodule AdventOfCode.Solution.Year2024.Day11 do
-  def part1(_input) do
+
+  def change_stone("0"), do: ["1"]
+  def change_stone(stone) do
+    if rem(String.length(stone), 2) == 0 do
+      String.split_at(stone, trunc(String.length(stone)/2)) |> Tuple.to_list() |> Enum.map(fn a -> String.to_integer(a) |> Integer.to_string() end)
+    else
+      [String.to_integer(stone) * 2024 |> Integer.to_string()]
+    end
   end
 
-  def part2(_input) do
+  def blink(stones) do
+    Enum.reduce(stones, %{}, fn {stone, count}, new_stones ->
+      change_stone(stone)
+      |> Enum.reduce(new_stones, fn new_stone, new_stones ->
+        Map.update(new_stones, new_stone, count, &(&1 + count))
+      end)
+    end)
+  end
+
+  def parse_input(input) do
+    input
+    |> String.trim_trailing()
+    |> String.split(" ", trim: true)
+  end
+
+  def part1(input) do
+    stones = parse_input(input)
+    |> Enum.reduce(%{}, fn stone, stones -> Map.update(stones, stone, 1, &(&1 + 1)) end)
+
+    1..25
+    |> Enum.reduce(stones, fn _, stones -> blink(stones) end)
+    |> Map.values()
+    |> Enum.sum()
+
+  end
+
+  def part2(input) do
+    stones = parse_input(input)
+    |> Enum.reduce(%{}, fn stone, stones -> Map.update(stones, stone, 1, &(&1 + 1)) end)
+
+    1..75
+    |> Enum.reduce(stones, fn _, stones -> blink(stones) end)
+    |> Map.values()
+    |> Enum.sum()
   end
 end
