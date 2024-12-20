1ed55dda87541412b72cd9ae9651f61a1de398db
Will McKenzie <willmckenzie@oinutter.co.uk>
2024-12-20 21:41:42 +0000

    Add 2023 days 7 and 17

--- lib/advent_of_code/solution/year_2023/day_07.ex
+++ lib/advent_of_code/solution/year_2023/day_07.ex
@@ -1,7 +1,179 @@
 defmodule AdventOfCode.Solution.Year2023.Day07 do
-  def part1(_input) do
+
+  use AdventOfCode.Solution.SharedParse
+
+  @card_scores %{
+    :A => 14,
+    :K => 13,
+    :Q => 12,
+    :J => 11,
+    :T => 10
+  }
+
+  @hand_scores %{
+    :five_of_a_kind => 700,
+    :four_of_a_kind => 600,
+    :full_house => 500,
+    :three_of_a_kind => 400,
+    :two_pair => 300,
+    :one_pair => 200,
+    :high_card => 100
+  }
+
+  def get_hand_result(cards, wildcard \\ false) do
+    card_counts = cards
+    |> Enum.reduce(%{}, fn card, counts ->
+      Map.update(counts, card, 1, &(&1+1))
+    end)
+
+    get_hand_type(card_counts, wildcard)
   end
 
-  def part2(_input) do
+  def get_card_score(card, wildcard \\ false) do
+    if wildcard and card === :J do
+      1
+    else
+     score = Map.get(@card_scores, card)
+
+     if score, do: score, else: card
+    end
+  end
+
+  defp get_hand_type(card_counts, wildcard) do
+    counts = Map.values(card_counts)
+    max_count = Enum.max(counts)
+
+    {counts, max_count, _} = if max_count !== 5 and wildcard do
+      cards = Map.to_list(card_counts)
+      |> Enum.filter(fn {_, count} -> count === max_count end)
+
+      {counts, max_count, card_counts} = if not (length(cards) === 1 and elem(hd(cards), 0) === :J) do
+        max_count = max_count + Map.get(card_counts, :J, 0)
+        card = elem(hd(cards), 0)
+
+        card_counts = Map.update!(card_counts, card, &(&1 + Map.get(card_counts, :J, 0)))
+        |> Map.put(:J, 0)
+        counts = Map.values(card_counts)
+        {counts, max_count, card_counts}
+      else
+        # J is max count so add to next highest score
+
+          next_highest = Map.to_list(card_counts)
+          |> Enum.filter( fn {card, _} ->
+            card !== :J
+          end)
+          |> Enum.sort(fn {_,a}, {_,b}  ->
+            if a < b do
+              true
+            else
+              false
+            end
+          end)
+          |> hd()
+
+          max_count = max_count + elem(next_highest, 1)
+          card_counts = Map.put(card_counts, :J, max_count)
+          |> Map.put(elem(next_highest,0), 0)
+          counts = Map.values(card_counts)
+          {counts, max_count, card_counts}
+      end
+      {counts, max_count, card_counts}
+    else
+      {counts, max_count, card_counts}
+    end
+
+    case max_count do
+      5 -> @hand_scores[:five_of_a_kind]
+      4 -> @hand_scores[:four_of_a_kind]
+      3 ->
+        if Enum.any?(counts, fn x -> x === 2 end) do
+          @hand_scores[:full_house]
+        else
+          @hand_scores[:three_of_a_kind]
+        end
+      2 ->
+        if Enum.count(counts, fn x -> x === 2 end) === 2 do
+          @hand_scores[:two_pair]
+        else
+          @hand_scores[:one_pair]
+        end
+      _ -> @hand_scores[:high_card]
+    end
+  end
+
+  def rank_hands(hands, wildcard \\ false) do
+    hands
+    |> Enum.map(fn hand ->
+      %{
+        :cards => hand.cards,
+        :score => get_hand_result(hand.cards, wildcard),
+      }
+    end)
+    |> Enum.sort(fn a, b ->
+      cond do
+        a.score < b.score ->
+          true
+
+        b.score < a.score ->
+          false
+
+        true ->
+          a.cards
+          |> Enum.with_index()
+          |> Enum.reduce_while(0, fn {card, i}, _ ->
+            a_score = get_card_score(card, wildcard)
+            b_score = get_card_score(Enum.at(b.cards, i), wildcard)
+
+            cond do
+              a_score < b_score ->
+                {:halt, true}
+
+              a_score > b_score ->
+                {:halt, false}
+
+              true ->
+                {:cont, 0}
+            end
+          end)
+      end
+    end)
+  end
+
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [cards, bet] = String.split(line, ~r/\s+/, trim: true, parts: 2)
+
+      {cards,
+        %{
+          :cards => String.split(cards, "", trim: true) |> Enum.map(fn char -> try do String.to_integer(char) rescue _ -> String.to_atom(char) end end),
+          :bet => String.to_integer(bet)
+        }}
+    end)
+    |> Map.new()
+  end
+
+  def part1(hands) do
+
+    hands
+    |> Map.values()
+    |> rank_hands()
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {result, i}, pot ->
+        bet = Map.get(hands, result.cards |> Enum.join("")).bet
+        pot + (bet * (i+1))
+    end)
+  end
+
+  def part2(hands) do
+    hands
+    |> Map.values()
+    |> rank_hands(true)
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {result, i}, pot ->
+        bet = Map.get(hands, result.cards |> Enum.join("")).bet
+        pot + (bet * (i+1))
+    end)
   end
 end
--- lib/advent_of_code/solution/year_2023/day_17.ex
+++ lib/advent_of_code/solution/year_2023/day_17.ex
@@ -1,7 +1,224 @@
 defmodule AdventOfCode.Solution.Year2023.Day17 do
-  def part1(_input) do
+
+  use AdventOfCode.Solution.SharedParse
+
+  defp get_neighbours({x, y, s}, map) do
+    neighbours = case s do
+      {:unknown, _} ->
+        [
+          {x + 1, y, {:east, 1}},
+          {x - 1, y, {:west, 1}},
+          {x, y + 1, {:south, 1}},
+          {x, y - 1, {:north, 1}}
+        ]
+
+      {:north, c} when c < 3 ->
+        [
+          {x, y - 1, {:north, c + 1}},
+          {x + 1, y, {:east, 1}},
+          {x - 1, y, {:west, 1}}
+        ]
+
+      {:north, _} ->
+        [
+          {x + 1, y, {:east, 1}},
+          {x - 1, y, {:west, 1}}
+        ]
+
+      {:south, c} when c < 3 ->
+        [
+          {x, y + 1, {:south, c + 1}},
+          {x + 1, y, {:east, 1}},
+          {x - 1, y, {:west, 1}}
+        ]
+
+      {:south, _} ->
+        [
+          {x + 1, y, {:east, 1}},
+          {x - 1, y, {:west, 1}}
+        ]
+
+      {:east, c} when c < 3 ->
+        [
+          {x + 1, y, {:east, c + 1}},
+          {x, y - 1, {:north, 1}},
+          {x, y + 1, {:south, 1}}
+        ]
+
+      {:east, _} ->
+        [
+          {x, y - 1, {:north, 1}},
+          {x, y + 1, {:south, 1}}
+        ]
+
+      {:west, c} when c < 3 ->
+        [
+          {x - 1, y, {:west, c + 1}},
+          {x, y - 1, {:north, 1}},
+          {x, y + 1, {:south, 1}}
+        ]
+
+      {:west, _} ->
+        [
+          {x, y - 1, {:north, 1}},
+          {x, y + 1, {:south, 1}}
+        ]
+    end
+    neighbours
+    |> Enum.filter(fn {x, y, _} -> Map.get(map, {x, y}) !== nil end)
   end
 
-  def part2(_input) do
+  defp get_neighbours2({x, y, s}, map) do
+    neighbours = case s do
+      {:unknown, _} ->
+        [
+          {x + 1, y, {:east, 1}},
+          {x - 1, y, {:west, 1}},
+          {x, y + 1, {:south, 1}},
+          {x, y - 1, {:north, 1}}
+        ]
+
+      {:north, c} when c < 4 ->
+        [
+          {x, y - 1, {:north, c + 1}},
+        ]
+
+      {:north, c} when c < 10 ->
+        [
+          {x, y - 1, {:north, c + 1}},
+          {x + 1, y, {:east, 1}},
+          {x - 1, y, {:west, 1}}
+        ]
+
+      {:north, _} ->
+        [
+          {x + 1, y, {:east, 1}},
+          {x - 1, y, {:west, 1}}
+        ]
+
+      {:south, c} when c < 4 ->
+        [
+          {x, y + 1, {:south, c + 1}},
+        ]
+
+      {:south, c} when c < 10 ->
+        [
+          {x, y + 1, {:south, c + 1}},
+          {x + 1, y, {:east, 1}},
+          {x - 1, y, {:west, 1}}
+        ]
+
+      {:south, _} ->
+        [
+          {x + 1, y, {:east, 1}},
+          {x - 1, y, {:west, 1}}
+        ]
+
+      {:east, c} when c < 4 ->
+        [
+          {x + 1, y, {:east, c + 1}},
+        ]
+
+        {:east, c} when c < 10 ->
+          [
+            {x + 1, y, {:east, c + 1}},
+            {x, y - 1, {:north, 1}},
+            {x, y + 1, {:south, 1}}
+          ]
+
+      {:east, _} ->
+        [
+          {x, y - 1, {:north, 1}},
+          {x, y + 1, {:south, 1}}
+        ]
+
+      {:west, c} when c < 4 ->
+        [
+          {x - 1, y, {:west, c + 1}},
+        ]
+
+        {:west, c} when c < 10 ->
+          [
+            {x - 1, y, {:west, c + 1}},
+            {x, y - 1, {:north, 1}},
+            {x, y + 1, {:south, 1}}
+          ]
+
+      {:west, _} ->
+        [
+          {x, y - 1, {:north, 1}},
+          {x, y + 1, {:south, 1}}
+        ]
+    end
+    neighbours
+    |> Enum.filter(fn {x, y, _} -> Map.get(map, {x, y}) !== nil end)
+  end
+
+  def parse(input) do
+    max_y = input |> String.split("\n", trim: true) |> length()
+    max_x = input |> String.split("\n", trim: true) |> hd() |> String.length()
+
+    map =
+      input
+      |> String.split("\n", trim: true)
+      |> Stream.with_index()
+      |> Enum.reduce([], fn {line, y}, acc ->
+        line
+        |> String.split("", trim: true)
+        |> Stream.with_index()
+        |> Enum.reduce(acc, fn {char, x}, acc ->
+          [{{x, y}, String.to_integer(char)} | acc]
+        end)
+      end)
+      |> Map.new()
+
+      {map, max_x, max_y}
+  end
+
+  def part1({map, max_x, max_y}) do
+
+    nbs = fn vertex -> get_neighbours(vertex, map) end
+    dist = fn _, {x, y, _} -> Map.get(map, {x, y}, :infinity) |> abs() end
+    h = fn {x, y, _} -> abs((max_x - x) + (max_y - y)) end
+    success = fn {x, y, _} -> x === max_x - 1 and y === max_y - 1 end
+
+    env = {
+      nbs,
+      dist,
+      h
+    }
+    Astar.astar(
+      env,
+      {0, 0, {:unknown, 0}},
+      success
+    )
+    |> Enum.reverse()
+    |> tl()
+    |> Enum.map(fn {x,y,_} -> Map.get(map, {x,y}) end)
+    |> Enum.sum()
+  end
+
+  def part2({map, max_x, max_y}) do
+
+    nbs = fn vertex -> get_neighbours2(vertex, map) end
+    dist = fn _, {x, y, _} -> Map.get(map, {x, y}, :infinity) |> abs() end
+    h = fn {x, y, _} -> abs((max_x - x) + (max_y - y)) end
+    success = fn {x, y, _} -> x === max_x - 1 and y === max_y - 1 end
+
+    env = {
+      nbs,
+      dist,
+      h
+    }
+    Astar.astar(
+      env,
+      {0, 0, {:unknown, 0}},
+      success
+    )
+    |> Enum.reverse()
+    |> tl()
+    |> Enum.map(fn {x,y,_} -> Map.get(map, {x,y}) end)
+    |> Enum.sum()
+
   end
 end
