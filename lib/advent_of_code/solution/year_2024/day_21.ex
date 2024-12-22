24ff6408b77a653807f40375935467021da27d89
Will McKenzie <willmckenzie@oinutter.co.uk>
2024-12-22 20:53:35 +0000

    add 2024 day 21 and 22

--- lib/advent_of_code/solution/year_2024/day_21.ex
+++ lib/advent_of_code/solution/year_2024/day_21.ex
@@ -1,7 +1,144 @@
 defmodule AdventOfCode.Solution.Year2024.Day21 do
-  def part1(_input) do
+  use Memoize
+
+  use AdventOfCode.Solution.SharedParse
+
+  @numeric_keypad %{
+    {0, 0} => "7",
+    {1, 0} => "8",
+    {2, 0} => "9",
+    {0, 1} => "4",
+    {1, 1} => "5",
+    {2, 1} => "6",
+    {0, 2} => "1",
+    {1, 2} => "2",
+    {2, 2} => "3",
+    {0, 3} => nil,
+    {1, 3} => "0",
+    {2, 3} => "A"
+  }
+
+  @directional_keypad %{
+    {0, 0} => nil,
+    {1, 0} => "^",
+    {2, 0} => "A",
+    {0, 1} => "<",
+    {1, 1} => "v",
+    {2, 1} => ">"
+  }
+
+  defp cheapest_dir_pad([], _, _, cheapest), do: cheapest
+  defmemop cheapest_dir_pad(queue, target, robots, cheapest) do
+    [{sx, sy, c} | rest] = queue
+    {tx, ty} = target
+    if {sx, sy} === {tx, ty} do
+      cost = cheapest_robot(["A" | c] |> Enum.reverse(), robots - 1)
+      cheapest = min(cost, cheapest)
+
+      cheapest_dir_pad(rest, {tx, ty}, robots, cheapest)
+    else
+      if {sx,sy} === {0,0} do
+        cheapest_dir_pad(rest, {tx, ty}, robots, cheapest)
+      else
+        rest =
+          cond do
+            ty > sy -> [{sx, sy + 1, ["v" | c]} | rest]
+            ty < sy -> [{sx, sy - 1, ["^" | c]} | rest]
+            true -> rest
+          end
+
+        rest =
+          cond do
+            tx > sx -> [{sx + 1, sy, [">" | c]} | rest]
+            tx < sx -> [{sx - 1, sy, ["<" | c]} | rest]
+            true -> rest
+          end
+
+        cheapest_dir_pad(rest, {tx, ty}, robots, cheapest)
+      end
+    end
   end
 
-  def part2(_input) do
+  defp cheapest_robot(presses, 1), do: presses |> length()
+  defp cheapest_robot(presses, robots) do
+    {_, total} = presses
+    |> Enum.reduce({{2,0}, 0}, fn press, {{sx,sy}, total} ->
+      {{tx, ty}, _} = Enum.find(@directional_keypad, fn {_, v} -> v === press end)
+      {
+        {tx, ty},
+        total + cheapest_dir_pad([{sx, sy, []}], {tx, ty}, robots, :infinity)
+      }
+    end)
+    total
+  end
+
+  defp cheapest_num_pad([], _, _, cheapest), do: cheapest
+  defmemop cheapest_num_pad(queue, target, keypads, cheapest) do
+    [{sx,sy,c}| rest] = queue
+    {tx, ty} = target
+    if {sx,sy} === {tx,ty} do
+      cost = cheapest_robot(["A" | c] |> Enum.reverse(), keypads)
+      cheapest = min(cost, cheapest)
+
+      cheapest_num_pad(rest, {tx,ty}, keypads, cheapest)
+    else
+      if {sx,sy} === {0,3} do
+        cheapest_num_pad(rest, {tx,ty}, keypads, cheapest)
+      else
+        rest =
+          cond do
+            ty > sy -> [{sx, sy + 1, ["v" | c]} | rest]
+            ty < sy -> [{sx, sy - 1, ["^" | c]} | rest]
+            true -> rest
+          end
+
+        rest =
+          cond do
+            tx > sx -> [{sx + 1, sy, [">" | c]} | rest]
+            tx < sx -> [{sx - 1, sy, ["<" | c]} | rest]
+            true -> rest
+          end
+
+        cheapest_num_pad(rest, {tx,ty}, keypads, cheapest)
+      end
+    end
+  end
+
+  defp cheapest_sequence(code, keypads) do
+    {_, total} = code
+    |> Enum.reduce({{2,3}, 0}, fn press, {{sx,sy}, total} ->
+      {{tx, ty}, _} = Enum.find(@numeric_keypad, fn {_, v} -> v === press end)
+      {
+        {tx, ty},
+        total + cheapest_num_pad([{sx,sy,[]}], {tx, ty}, keypads, :infinity)
+      }
+    end)
+    total
+  end
+
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+  end
+
+  defp get_score(codes, keypads) do
+    codes
+    |> Enum.map(fn code ->
+      keys = code |> String.split("", trim: true)
+
+      sequence_length = cheapest_sequence(keys, keypads)
+      numeric_value = code |> String.replace("A", "") |> String.to_integer()
+
+      sequence_length * numeric_value
+    end)
+    |> Enum.sum()
+  end
+
+  def part1(codes) do
+    get_score(codes, 3)
+  end
+
+  def part2(codes) do
+    get_score(codes, 26)
   end
 end
--- lib/advent_of_code/solution/year_2024/day_22.ex
+++ lib/advent_of_code/solution/year_2024/day_22.ex
@@ -1,7 +1,81 @@
 defmodule AdventOfCode.Solution.Year2024.Day22 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+  use Memoize
+
+  def mix(a, b) do
+    Bitwise.bxor(a, b)
   end
 
-  def part2(_input) do
+  def prune(a) do
+    rem(a, 16_777_216)
+  end
+
+  def generate_secret_number(n) do
+    new_n = prune(mix(n * 64, n))
+    new_n = prune(mix(floor(new_n / 32), new_n))
+    prune(mix(new_n * 2048, new_n))
+  end
+
+  @spec parse(binary()) :: list()
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  @spec part1(any()) :: nil
+  def part1(numbers) do
+    numbers
+    |> Enum.map(fn n ->
+      1..2000
+      |> Enum.reduce(n, fn _, n ->
+        generate_secret_number(n)
+      end)
+    end)
+    |> Enum.sum()
+  end
+
+  def part2(numbers) do
+    sequences =
+      numbers
+      |> Enum.map(fn n ->
+        1..2000
+        |> Enum.reduce([n], fn _, n ->
+          [generate_secret_number(hd(n)) | n]
+        end)
+      end)
+      |> Enum.map(fn sequence ->
+        sequence
+        |> Enum.map(fn n ->
+          rem(n, 10)
+        end)
+        |> Enum.reverse()
+      end)
+
+    bananas =
+      sequences
+      |> Enum.reduce(%{}, fn sequence, bananas ->
+        {bananas, _} =
+          sequence
+          |> Enum.chunk_every(5, 1, :discard)
+          |> Enum.reduce({bananas, MapSet.new()}, fn [a, b, c, d, e], {bananas, seen} ->
+            key = {b - a, c - b, d - c, e - d}
+
+            if not MapSet.member?(seen, key) do
+              {
+                Map.update(bananas, key, e, &(&1 + e)),
+                MapSet.put(seen, key)
+              }
+            else
+              {bananas, seen}
+            end
+          end)
+
+        bananas
+      end)
+
+    bananas
+    |> Map.values()
+    |> Enum.max()
   end
 end
